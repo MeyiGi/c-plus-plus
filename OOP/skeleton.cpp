e7c625d8666429b418a0906258875d952bc5a00c
MeyiGi <130828110+MeyiGi@users.noreply.github.com>
2024-04-02 09:16:11 +0600

    Update skeleton.cpp

--- OOP/skeleton.cpp
+++ OOP/skeleton.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 using namespace std;
 
 class Student
@@ -16,6 +17,11 @@ public:
         this->name = name;
         this->age = age;
         this->scholarship = scholarship;
+    }
+    // == operator
+    bool operator==(const Student &student) const
+    {
+        return (name == student.name && age == student.age && scholarship == student.scholarship);
     }
 
     // Setters
@@ -49,10 +55,26 @@ public:
     // Destructor
     ~Student()
     {
-        cout << "Destructor making something";
+        cout << "Destructor making something" << endl;
     }
 };
 
+class MyCollection
+{
+public:
+    list<Student> students;
+
+    void operator+=(Student &student)
+    {
+        this->students.push_back(student);
+    }
+    void operator-=(Student &student)
+    {
+        this->students.remove(student);
+    }
+};
+
+// cout operators for classes
 ostream &operator<<(ostream &COUT, Student &student)
 {
     cout << "Name: " << student.getName() << endl;
@@ -61,6 +83,14 @@ ostream &operator<<(ostream &COUT, Student &student)
 
     cout << endl;
     return COUT;
+}
+
+ostream &operator<<(ostream &COUT, MyCollection &collection)
+{
+    for (Student &student : collection.students)
+    {
+        COUT << student;
+    }
 }
 
 int main()
